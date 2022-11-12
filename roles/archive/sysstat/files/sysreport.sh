b1d5bd21095674de03e72e88d6b12b22e1b66c82
XakV <zachvatwork@gmail.com>
2022-11-12 16:30:48 -0500

    removed archive stuff, encrypted other thing

--- roles/archive/sysstat/files/sysreport.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/bin/sadf -d /var/log/sa/sa$(date "+%d") -- -u -r -dp -n DEV >> /tmp/sysreport-$(date "+%m-%d").txt
-/usr/bin/echo /tmp/sysreport-$(date "+%d").txt | /usr/bin/mutt -s sysreport jebel@muckrobin
